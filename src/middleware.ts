a974795fc631b93fd4abb3e87ebf02bc430b29f7
Andrii <pasiura.andriy@lll.kpi.ua>
2025-11-20 22:55:36 +0200

    chore: protect auth pages with middleware

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import type { NextRequest } from 'next/server';
+
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+import { PRIVATE_ROUTES, PUBLIC_ROUTES } from '@/constants';
+
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const token = await getToken({ req });
+
+  const isAuthPage = pathname === PUBLIC_ROUTES.SIGN_IN || pathname === PUBLIC_ROUTES.SIGN_UP;
+  const isVerificationPage = pathname === PUBLIC_ROUTES.VERIFICATION;
+
+  if ((token && isAuthPage) || (isVerificationPage && token?.isVerified)) {
+    return NextResponse.redirect(new URL(PRIVATE_ROUTES.DIAGRAMS, req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/',
+    '/signup',
+    '/verification',
+    '/diagrams/:path*',
+    '/profile/:path*',
+    '/settings/:path*',
+  ],
+};
