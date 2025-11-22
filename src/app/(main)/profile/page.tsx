29a7085eebb9a40837052303ba276933593747ba
Andrii <pasiura.andriy@lll.kpi.ua>
2025-11-22 21:31:21 +0200

    feat(profile): fix formatting

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -2,12 +2,12 @@ import { getUser } from '@/actions';
 import { Profile } from '@/features/user/components/profile';
 
 async function ProfilePage() {
-    const user = await getUser();
-  
-    if (!user) {
-      return null
-    }
-  return <Profile user={user}/>;
+  const user = await getUser();
+
+  if (!user) {
+    return null;
+  }
+  return <Profile user={user} />;
 }
 
 export default ProfilePage;
